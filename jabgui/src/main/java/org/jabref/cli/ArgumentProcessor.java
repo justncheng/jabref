1aac684fc644cb1d7eb2d26775e40b3db6a36741
Aman Agnihotri <90140219+aman-agnihotri-02@users.noreply.github.com>
2026-03-07 23:49:42 -0500

    Update ArgumentProcessor.java
    Fix #3:  Use format specifier instead of string concatenation in ArgumentProcessor.java
    
    Resolved SonarCloud code smell (java:S3457) by replacing string concatenation with a proper format specifier.

--- jabgui/src/main/java/org/jabref/cli/ArgumentProcessor.java
+++ jabgui/src/main/java/org/jabref/cli/ArgumentProcessor.java
@@ -45,13 +45,13 @@ public class ArgumentProcessor {
         guiNeeded = true;
 
         if ((startupMode == Mode.INITIAL_START) && cli.isVersionHelpRequested()) {
-            System.out.printf(BuildInfo.JABREF_BANNER + "%n", new BuildInfo().version);
+            System.out.printf("%s%n", BuildInfo.JABREF_BANNER + new BuildInfo().version);
             guiNeeded = false;
             return List.of();
         }
 
         if ((startupMode == Mode.INITIAL_START) && cli.isUsageHelpRequested()) {
-            System.out.printf(BuildInfo.JABREF_BANNER + "%n", new BuildInfo().version);
+            System.out.printf("%s%n", BuildInfo.JABREF_BANNER + new BuildInfo().version);
             System.out.println(cli.getUsageMessage());
 
             guiNeeded = false;
